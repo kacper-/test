1ac058c00603eeee97bb219c247fcd4debb94a1e
Kacper Marczewski <kacper.marczewski@gmail.com>
2021-05-23 20:21:22 +0200

    primes

---
+++ src/main/java/comt/primes/Primes.java
@@ -0,0 +1,33 @@
+package comt.primes;
+
+import java.util.Date;
+
+public class Primes {
+    public static void main(String[] args) {
+        long from = 10000000000L;
+        long to = 10000001000L;
+        int count = (int) (to - from);
+        int[] a = new int[count];
+
+        long start = new Date().getTime();
+
+        double m = Math.sqrt(count + from);
+        for (long j = from; j < from + count; j++) {
+            for (long i = 2; i <= m; i++) {
+                if ((j % i) == 0) {
+                    a[(int) (j - from)] = 1;
+                    break;
+                }
+            }
+        }
+
+        long stop = new Date().getTime();
+
+        for (int i = 0; i < count; i++) {
+            if (a[i] == 0) {
+                System.out.println(from + i);
+            }
+        }
+        System.out.println("Time: " + (stop - start) + " ms");
+    }
+}
