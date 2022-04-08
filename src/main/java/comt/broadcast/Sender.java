64d821c5d1a50f70589f4507467aad060db22fde
Kacper Marczewski <kacper.marczewski@gmail.com>
2022-04-08 16:13:33 +0200

    hazelcast added

--- src/main/java/comt/broadcast/Sender.java
+++ src/main/java/comt/broadcast/Sender.java
@@ -9,7 +9,7 @@ public class Sender implements Runnable {
     @Override
     public void run() {
         try {
-            InetAddress address = InetAddress.getByName("192.168.64.255");
+            InetAddress address = InetAddress.getByName("192.168.1.255");
             DatagramSocket socket = new DatagramSocket();
             socket.setBroadcast(true);
             byte[] buffer = "test UDP broadcast message".getBytes();
