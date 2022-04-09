7f3e4bf50ec68da1347dd238cd79ddccfd5480ab
Kacper Marczewski <kacper.marczewski@gmail.com>
2022-04-09 18:43:53 +0200

    hazelcast queue client read and write

---
+++ src/main/java/comt/hazelcast/ClientTake.java
@@ -0,0 +1,23 @@
+package comt.hazelcast;
+
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.core.HazelcastInstance;
+
+import java.util.concurrent.BlockingQueue;
+
+public class ClientTake {
+
+    public static void main(String[] args) throws Exception {
+        ClientConfig clientConfig = new ClientConfig();
+        clientConfig.getNetworkConfig().addAddress("ubuntu1");
+
+        HazelcastInstance client = HazelcastClient.newHazelcastClient(clientConfig);
+        System.out.println(clientConfig.toString());
+
+        BlockingQueue<String> queue = client.getQueue("queue");
+        System.out.println(queue.take());
+
+        HazelcastClient.shutdownAll();
+    }
+}
