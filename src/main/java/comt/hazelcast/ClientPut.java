a158d0fb4edf01585988b9fb502fd82db9bc301c
Kacper Marczewski <kacper.marczewski@gmail.com>
2022-04-09 20:32:31 +0200

    hazelcast map client read and write

--- src/main/java/comt/hazelcast/ClientPut.java
+++ src/main/java/comt/hazelcast/ClientPut.java
@@ -1,22 +1,39 @@
 package comt.hazelcast;
 
+import com.hazelcast.cache.impl.HazelcastServerCachingProvider;
 import com.hazelcast.client.HazelcastClient;
 import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.config.NearCacheConfig;
 import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.map.IMap;
 
+import javax.cache.Cache;
+import javax.cache.CacheManager;
+import javax.cache.Caching;
+import javax.cache.configuration.MutableConfiguration;
+import javax.cache.spi.CachingProvider;
 import java.util.concurrent.BlockingQueue;
+
+import static com.hazelcast.cache.HazelcastCachingProvider.propertiesByInstanceItself;
 
 public class ClientPut {
 
     public static void main(String[] args) throws Exception {
+        NearCacheConfig nearCacheConfig = new NearCacheConfig("test");
+
         ClientConfig clientConfig = new ClientConfig();
         clientConfig.getNetworkConfig().addAddress("ubuntu1");
+        clientConfig.addNearCacheConfig(nearCacheConfig);
 
         HazelcastInstance client = HazelcastClient.newHazelcastClient(clientConfig);
         System.out.println(clientConfig.toString());
 
-        BlockingQueue<String> queue = client.getQueue("queue");
-        queue.put("Hello!");
+        IMap<Object, Object> map = client.getMap("default");
+        map.put("aaa", "bbb");
+
+        //BlockingQueue<String> queue = client.getQueue("queue");
+        //queue.put("Hello!");
+
         System.out.println("Message sent by Hazelcast Client!");
 
         HazelcastClient.shutdownAll();
--- src/main/java/comt/hazelcast/ClientTake.java
+++ src/main/java/comt/hazelcast/ClientTake.java
@@ -1,9 +1,16 @@
 package comt.hazelcast;
 
+import com.hazelcast.cache.impl.HazelcastServerCachingProvider;
 import com.hazelcast.client.HazelcastClient;
 import com.hazelcast.client.config.ClientConfig;
 import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.map.IMap;
 
+import javax.cache.Cache;
+import javax.cache.CacheManager;
+import javax.cache.Caching;
+import javax.cache.configuration.MutableConfiguration;
+import javax.cache.spi.CachingProvider;
 import java.util.concurrent.BlockingQueue;
 
 public class ClientTake {
@@ -15,8 +22,14 @@ public class ClientTake {
         HazelcastInstance client = HazelcastClient.newHazelcastClient(clientConfig);
         System.out.println(clientConfig.toString());
 
-        BlockingQueue<String> queue = client.getQueue("queue");
-        System.out.println(queue.take());
+        CachingProvider cachingProvider = Caching.getCachingProvider(HazelcastServerCachingProvider.class.getName());
+        CacheManager cacheManager = cachingProvider.getCacheManager();
+
+        IMap<Object, Object> map = client.getMap("default");
+        System.out.println(map.get("aaa"));
+
+        //BlockingQueue<String> queue = client.getQueue("queue");
+        //System.out.println(queue.take());
 
         HazelcastClient.shutdownAll();
     }
