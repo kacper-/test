8f60a9a2347e381488ccc3c14659dc28e5a14b75
Kacper Marczewski <kacper.marczewski@gmail.com>
2022-07-13 20:47:09 +0200

    better distribution check

--- src/main/java/comt/dist/Dist.java
+++ src/main/java/comt/dist/Dist.java
@@ -3,16 +3,16 @@ package comt.dist;
 import java.util.Random;
 
 public class Dist {
-    public static final int ITER = 10000000;
+    public static final int ITER = 100000000;
     static public void main(String argv[]) {
-        int[] t = new int[5];
+        int[] t = new int[10];
         for(int i=0;i<t.length;i++)
             t[i]=0;
         for(int i=0;i<ITER;i++)
-            t[new Random().nextInt(5)]++;
+            t[new Random().nextInt(10)]++;
         for(int i=0;i<t.length;i++) {
-            int diff = Math.abs((ITER/5) - t[i]);
-            System.out.println(String.format("%d = %d diff = %f", i + 1, t[i], ((float)diff) / ((float) ITER/5)));
+            int diff = Math.abs((ITER/10) - t[i]);
+            System.out.println(String.format("%d = %d \tdiff = %f", i + 1, t[i], ((float)diff) / ((float) ITER/10)));
         }
     }
 }
