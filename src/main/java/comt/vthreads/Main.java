d787bb59dc95a87853410f3ed07233218e43c265
Kacper Marczewski <kacper.marczewski@gmail.com>
2024-12-06 19:01:08 +0100

    test for virtual threads

---
+++ src/main/java/comt/vthreads/Main.java
@@ -0,0 +1,50 @@
+package comt.vthreads;
+
+import java.util.Date;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Main {
+
+    static AtomicInteger threadCounter = new AtomicInteger(0);
+
+    static int k;
+
+    public static void main(String args[]) {
+        new Thread(() -> {
+            long start = new Date().getTime();
+            while (threadCounter.get() < 10) {
+                System.out.println("count = " + Thread.activeCount());
+                try {
+                    Thread.sleep(250);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            long stop = new Date().getTime();
+            System.out.println("time = " + (stop - start));
+        }).start();
+
+        for (k = 0; k < 10; k++) {
+            start(k);
+        }
+    }
+
+    static void start(final int k) {
+        Thread.ofPlatform().start(() -> {
+            int n = k;
+            double a = (n + 2);
+            for (int i = 0; i < 50; i++) {
+                try {
+                    for (int j = 0; j < 10000000; j++) {
+                        a += Math.log(a);
+                    }
+                    Thread.sleep(8 + (12 * (long) n));
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            System.out.println("Thread " + n + " finished with = " + a);
+            threadCounter.incrementAndGet();
+        });
+    }
+}
