709e1320797d10f9d5e2e05e6323c3d7d380976d
Kacper Marczewski <kacper.marczewski@gmail.com>
2025-03-23 21:01:35 +0100

    better generic discovery code

--- src/main/java/comt/type/Main.java
+++ src/main/java/comt/type/Main.java
@@ -7,19 +7,31 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Type t1 = getGenericRuntimeType(new Wrapper<List<String>>() {});
-        System.out.println(t1);
+        try {
+            Class<?> c = getGenericRuntimeType(new Wrapper<String>() {
+            });
+            System.out.println(c);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public static <T> Type getGenericRuntimeType(Wrapper<T> wrapper) {
+    public static <T> Class<?> getGenericRuntimeType(Wrapper<T> wrapper) throws ClassNotFoundException {
         Type type = wrapper.getClass().getGenericSuperclass();
         if (type == null) {
             return null;
         }
 
         if (type instanceof ParameterizedType) {
-            Type[] types = ((ParameterizedType)type).getActualTypeArguments();
-            return types[0];
+            Type[] types = ((ParameterizedType) type).getActualTypeArguments();
+            String fullName = types[0].toString();
+            if (fullName.contains("<")) {
+                String outerName = fullName.substring(0, fullName.indexOf("<"));
+                return Class.forName(outerName);
+            } else {
+                String innerName = fullName.substring(fullName.indexOf(" ") + 1);
+                return Class.forName(innerName);
+            }
         }
         return null;
     }
